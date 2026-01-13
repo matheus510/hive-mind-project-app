ed67969a8fe9f669f66f327750a52ffe0e587b41
Matheus <matheus510.fonseca@gmail.com>
2026-01-12 23:29:10 -0300

    feat: add mock number system

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,17 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const string = this.createNumbers(4).toString();
+    return string;
+  }
+  createNumbers(playerCount: number = 4): number[] {
+    let numbers: number[] = []
+    while (numbers.length < playerCount) {
+      let randomNumber = Math.floor(Math.random() * 100)
+      if (!numbers.includes(randomNumber)) {
+        numbers.push(randomNumber)
+      }
+    }
+    return numbers
   }
 }
